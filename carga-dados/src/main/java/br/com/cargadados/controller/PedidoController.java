ad2d42ca4d852dcc534300bdfd6e49c2254a42a5
cleversonkleber <cleversonkleber@gmail.com>
2023-09-19 13:13:33 -0300

    Apresentação de dados

--- carga-dados/src/main/java/br/com/cargadados/controller/PedidoController.java
+++ carga-dados/src/main/java/br/com/cargadados/controller/PedidoController.java
@@ -1,17 +1,33 @@
 package br.com.cargadados.controller;
 
 
+import br.com.cargadados.model.Pedido;
+import br.com.cargadados.repository.PedidoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class PedidoController {
 
+    @Autowired
+    PedidoRepository repository;
+
     @GetMapping("/pedidos")
-    public String index(){
-        return "pedidos/index";
+    public ModelAndView index(){
+
+
+        List<Pedido> pedidos = (List<Pedido>) this.repository.findAll();
+
+        ModelAndView mv = new ModelAndView("pedidos/index");
+        mv.addObject("pedidos",pedidos);
+
+        return mv;
 
     }
 
