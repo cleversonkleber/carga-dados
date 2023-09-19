5218d7ace7d5b933011f94d97a4ff3330fe000b5
cleversonkleber <cleversonkleber@gmail.com>
2023-09-18 21:26:31 -0300

    Carga dados

---
+++ carga-dados/src/main/java/br/com/cargadados/CargaDadosApplication.java
@@ -0,0 +1,13 @@
+package br.com.cargadados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargaDadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CargaDadosApplication.class, args);
+	}
+
+}
---
+++ carga-dados/src/test/java/br/com/cargadados/CargaDadosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cargadados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargaDadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
